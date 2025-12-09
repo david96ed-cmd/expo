7be796bbaf26b22263eb82b3508efb0cdb37f407
Douglas Lowder <douglowder@mac.com>
2025-12-08 17:06:20 -0800

    State change listener on Android

--- packages/expo-updates/e2e/fixtures/UpdatesE2ETestModule.kt
+++ packages/expo-updates/e2e/fixtures/UpdatesE2ETestModule.kt
@@ -1,36 +1,81 @@
 package expo.modules.updates
 
+import android.os.Bundle
 import expo.modules.kotlin.Promise
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
+import expo.modules.kotlin.types.Enumerable
+import expo.modules.updatesinterface.UpdatesControllerRegistry
+import expo.modules.updatesinterface.UpdatesEnabledInterface
+import expo.modules.updatesinterface.UpdatesStateChangeListener
+import expo.modules.updatesinterface.statemachine.UpdatesStateEvent
 
-class UpdatesE2ETestModule : Module() {
+interface UpdatesEnabledTestingInterface: UpdatesEnabledInterface {
+  fun clearInternalAssetsFolderAsync(promise: Promise)
+  fun readInternalAssetsFolderAsync(promise: Promise)
+}
+
+class UpdatesE2ETestModule : Module(), UpdatesStateChangeListener {
+  private var hasListener: Boolean = false
+
   override fun definition() = ModuleDefinition {
     Name("ExpoUpdatesE2ETest")
 
+    Events<UpdatesE2EEvent>()
+
+    OnStartObserving(UpdatesE2EEvent.StateChange) {
+      hasListener = true
+    }
+
+    OnStopObserving(UpdatesE2EEvent.StateChange) {
+      hasListener = false
+    }
+
     AsyncFunction("clearInternalAssetsFolderAsync") { promise: Promise ->
-      try {
-        val assetsFolder = UpdatesController.instance.updatesDirectory
-        assetsFolder!!.deleteRecursively()
-        promise.resolve(null)
-      } catch (e: Throwable) {
-        promise.reject("ERR_E2E_TEST", null, e)
+      UpdatesControllerRegistry.controller?.get()?.let {
+        if (it is UpdatesEnabledTestingInterface) {
+          it.clearInternalAssetsFolderAsync(promise)
+        }
       }
     }
 
     AsyncFunction("readInternalAssetsFolderAsync") { promise: Promise ->
-      try {
-        val assetsFolder = UpdatesController.instance.updatesDirectory
-        if (!assetsFolder!!.exists()) {
-          promise.resolve(0)
-        } else {
-          val count = assetsFolder.walk()
-            .count() - 1 // subtract one for the folder itself, which is included in walk()
-          promise.resolve(count)
+      UpdatesControllerRegistry.controller?.get()?.let {
+        if (it is UpdatesEnabledTestingInterface) {
+          it.readInternalAssetsFolderAsync(promise)
         }
-      } catch (e: Throwable) {
-        promise.reject("ERR_E2E_TEST", null, e)
       }
     }
   }
+
+  override fun updatesStateDidChange(event: UpdatesStateEvent) {
+    if (hasListener) {
+      val payload = Bundle()
+      payload.putString("type", event.type.toString())
+      when(event) {
+        is UpdatesStateEvent.CheckCompleteWithUpdate -> {
+          val manifest = event.manifest
+          val manifestBundle = Bundle()
+          manifestBundle.putString("id", manifest.getString("id"))
+          payload.putBundle("manifest", manifestBundle)
+        }
+        is UpdatesStateEvent.DownloadCompleteWithUpdate -> {
+          val manifest = event.manifest
+          val manifestBundle = Bundle()
+          manifestBundle.putString("id", manifest.getString("id"))
+          payload.putBundle("manifest", manifestBundle)
+        }
+        else -> {}
+      }
+      sendEvent(E2E_EVENT_NAME, payload)
+    }
+  }
+
+  companion object {
+    const val E2E_EVENT_NAME = "Expo.updatesE2EStateChangeEvent"
+  }
+}
+
+enum class UpdatesE2EEvent(val eventName: String) : Enumerable {
+  StateChange(UpdatesE2ETestModule.E2E_EVENT_NAME)
 }
